6608825827b43ff55963c77a4a6e885badd4b581
alioop1 <61251677+alioop1@users.noreply.github.com>
2026-03-14 21:16:03 +0200

    refactor(domain): delete old usecase files (merged into UseCases.kt)

--- app/src/main/java/com/luminastreams/tv/domain/usecase/FetchSubtitlesUseCase.kt
+++
@@ -1,7 +0,0 @@
-package com.luminastreams.tv.domain.usecase
-
-class FetchSubtitlesUseCase(private val scraper: com.luminastreams.tv.data.remote.SubtitleScraper) {
-    suspend operator fun invoke(imdbId: String, lang: String): Result<ByteArray> {
-        return scraper.fetchSubtitleInMemory(imdbId, lang)
-    }
-}
